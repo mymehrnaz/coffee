e4c97a7a91111e80b9addfe6fc1ffdff623e6f63
mymehrnaz <165458618+mymehrnaz@users.noreply.github.com>
2025-10-24 18:18:42 +0330

    images added

--- src/main.js
+++ src/main.js
@@ -2,25 +2,25 @@ function changeTab(category) {
     // داده‌های نمونه با جزئیات کارت (می‌تونی تصاویر واقعی بذاری)
     const foods = {
         seafood: [
-              { name: "Espresso", desc: "Ramen, lotus root, daikon radish...", img: "image/espresso.jpg", price: "$5", cal: "300ml 359cal" },
-  { name: "Americano", desc: "Ramen, lotus root, daikon radish...", img: "image/americano.jpg", price: "$5", cal: "300ml 359cal" },
+              { name: "Espresso", desc: "Ramen, lotus root, daikon radish...", img: "image/dagh.webp", price: "$5", cal: "300ml 359cal" },
+  { name: "Americano", desc: "Ramen, lotus root, daikon radish...", img: "image/usa.webp", price: "$5", cal: "300ml 359cal" },
 
-            { name: "Latte", desc: "Ramen, lotus root, daikon radish...", img: "image/latte.jpg", price: "$5", cal: "300ml 359cal" },
-            { name: "Capuccino", desc: "Ramen, vegetable broth...", img: "image/capocciano.jpg", price: "$6", cal: "Available +6" }
+            { name: "Latte", desc: "Ramen, lotus root, daikon radish...", img: "image/lattes.webp", price: "$5", cal: "300ml 359cal" },
+            { name: "Capuccino", desc: "Ramen, vegetable broth...", img: "image/capoch.webp", price: "$6", cal: "Available +6" }
         ],
         veggie: [
-              { name: "Vanilla Latte", desc: "Ramen, lotus root, daikon radish...", img: "image/vannila.jpg", price: "$5", cal: "300ml 359cal" },
-  { name: "Cafe Latte", desc: "Ramen, lotus root, daikon radish...", img: "image/cafe.jpg", price: "$5", cal: "300ml 359cal" },
+              { name: "Vanilla Latte", desc: "Ramen, lotus root, daikon radish...", img: "image/vanili.webp", price: "$5", cal: "300ml 359cal" },
+  { name: "Cafe Latte", desc: "Ramen, lotus root, daikon radish...", img: "image/icee.webp", price: "$5", cal: "300ml 359cal" },
 
-            { name: "Iced Americano", desc: "Ramen, pumpkin, greens...", img: "image/americ.jpg", price: "$5", cal: "300ml 359cal" },
-            { name: "Salted Caramel", desc: "Ramen, vegetable broth...", img: "image/caramel.jpg", price: "$4", cal: "Available +3" }
+            { name: "Iced Americano", desc: "Ramen, pumpkin, greens...", img: "image/american.webp", price: "$5", cal: "300ml 359cal" },
+            { name: "Salted Caramel", desc: "Ramen, vegetable broth...", img: "image/karam.webp", price: "$4", cal: "Available +3" }
         ],
         meat: [
-              { name: "Yousei no miru", desc: "Ramen, lotus root, daikon radish...", img: "https://via.placeholder.com/200?text=Seafood1", price: "$5", cal: "300ml 359cal" },
-  { name: "Yousei no miru", desc: "Ramen, lotus root, daikon radish...", img: "https://via.placeholder.com/200?text=Seafood1", price: "$5", cal: "300ml 359cal" },
+              { name: "peanut butter chocolate shake", desc: "Ramen, lotus root, daikon radish...", img: "image/pinut.webp", price: "$5", cal: "300ml 359cal" },
+  { name: "Morning protain shake", desc: "Ramen, lotus root, daikon radish...", img: "image/morning.webp", price: "$5", cal: "300ml 359cal" },
 
-            { name: "Chicken Soup", desc: "With noodles and veggies...", img: "https://via.placeholder.com/200?text=Meat1", price: "$7", cal: "400cal" },
-            { name: "Beef Ramen", desc: "Spicy and hearty...", img: "https://via.placeholder.com/200?text=Meat2", price: "$8", cal: "500cal" }
+            { name: "Protain coffee", desc: "With noodles and veggies...", img: "image/protin.webp", price: "$7", cal: "400cal" },
+            { name: "Wellness smoothie", desc: "Spicy and hearty...", img: "image/smoti.webp", price: "$8", cal: "500cal" }
         ],
 herbal: [ 
      { name: "Herbal Tea", desc: "Green tea with herbs...", img: "https://via.placeholder.com/200?text=Herbal1", price: "$4", cal: "200ml 150cal" },
